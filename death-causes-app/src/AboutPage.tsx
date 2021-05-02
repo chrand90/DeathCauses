809d6e73df1a7dd17c60ed5bab038064dfbadd94
chrand90 <chrand90@gmail.com>
2021-05-02 12:37:18 +0200

    fixed mathjax and about page

---
+++ death-causes-app/src/AboutPage.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import './AboutPage.css'
+var html = require('./resources/intro.nb.html')
+var template = { __html: html }
+const MathJaxLoader = require("./components/MathJax");
+
+
+export class AboutPage extends React.Component<any, any> {
+
+    constructor(props: any ){
+        super(props);
+        const mathjaxLoader = new MathJaxLoader();
+        this.state = {
+            loaded: false,
+            mathjax: mathjaxLoader,
+            initialValue: 1
+        };
+      }
+    
+      onFirstMathJax(){
+          this.setState({
+            loaded: true
+          });
+      }
+    
+      componentDidMount() {
+          this.state.mathjax.initiateMathJax(this.onFirstMathJax.bind(this)); 
+      }
+    
+      componentDidUpdate(prevProps: any, prevState: any, snapshot: any) {
+          this.state.mathjax.update();
+      }
+    
+      onIncrementClick(){
+        let initialValue = this.state.initialValue;
+        this.setState({
+          initialValue: initialValue + 1
+        })
+      }
+
+
+    render() {
+        return (
+            <Container>
+                <div>
+                    <span dangerouslySetInnerHTML={template} />
+                </div>
+            </Container>
+        )
+    }
+}
