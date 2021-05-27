db78eee8643df8409809bb1bbf86bae4e1eb8b64
svendvn <svend1991@msn.com>
2021-05-27 16:48:47 +0200

    Forgot to organize imports in loaded data store

--- death-causes-app/src/stores/LoadedDataStore.tsx
+++ death-causes-app/src/stores/LoadedDataStore.tsx
@@ -1,15 +1,8 @@
-import React from "react";
-import { makeObservable, observable, computed, action } from "mobx";
-import Factors, { FactorAnswers, FactorAnswerUnitScalings } from "../models/Factors";
-import { InputValidity } from "../models/FactorAbstract";
-import InputJson from "../models/FactorJsonInput";
-import FactorInputStore from "./FactorInputStore";
-import QuestionProgressStore from "./QuestionProgressStore";
-import factorDatabase from "../resources/FactorDatabase.json";
-import { loadFactors, LoadedFactors, loadRelationLinks, LoadedRelationLinks, LoadedCauseData, loadCauseData } from "./DataLoading";
-import RelationLinks, { RelationLinkJson } from "../models/RelationLinks";
-import relationLinkFile from "../resources/Relations.json";
 import DeathCause, { RawDeathCauseJson, RiskFactorGroupsContainer } from "../components/database/Deathcause";
+import InputJson from "../models/FactorJsonInput";
+import Factors from "../models/Factors";
+import RelationLinks, { RelationLinkJson } from "../models/RelationLinks";
+import { loadCauseData, LoadedCauseData, LoadedFactors, LoadedRelationLinks, loadFactors, loadRelationLinks } from "./DataLoading";
 
 export default class LoadedDataStore {
   factors: Factors;
@@ -40,7 +33,7 @@ export default class LoadedDataStore {
             this.factors=loadedFactors.factors
             this.rawFactorInput=loadedFactors.rawFactorInput;
             this.factorOrder=loadedFactors.factors.getSortedOrder();
-            return "ds"
+            return
           }).then(() => {
               return loadRelationLinks()
           })
