f13adf8211876f41526a386a59ab76a4072faf15
chrand90 <chrand90@gmail.com>
2021-08-06 22:01:26 +0200

    commented lollipopcharts

--- death-causes-app/src/components/SummaryView.tsx
+++ death-causes-app/src/components/SummaryView.tsx
@@ -82,12 +82,12 @@ export class SummaryViewWithoutStore extends React.Component<SummaryViewProps> {
                         </CardBody>
                     </Card>
                 </Col>
-                <Row className="mx-auto my-1" style={{ width: "70%" }}>
+                {/* <Row className="mx-auto my-1" style={{ width: "70%" }}>
                     <Col className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
                         <Card className="bg-light " style={{}} >
                             <CardHeader><h5 style={{ fontWeight: 600 }}>Years lost per cause</h5></CardHeader>
                             <LollipopChart data={summaryViewData.yearsLostToDeathCauses} descriptions={this.props.store.loadedDataStore.descriptions} formatting={probabilityLollipopChartFormatter}/>
-                            {/* <table style={{ width: "60%", margin: "auto" }}>
+                            <table style={{ width: "60%", margin: "auto" }}>
                                 <thead>
                                     <th style={{ textAlign: "left" }}>Death cause</th>
                                     <th style={{ textAlign: "right" }}>Years lost</th>
@@ -100,16 +100,16 @@ export class SummaryViewWithoutStore extends React.Component<SummaryViewProps> {
                                         })
                                     }
                                 </tbody>
-                            </table> */}
+                            </table>
                         </Card>
                     </Col>
                     <Col className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <Card className="bg-light" style={{}}> {/*backgroundColor: 'rgba(50, 50, 200, 0.1)'*/}
+                        <Card className="bg-light" style={{}}> 
                             <CardHeader><h5 style={{ fontWeight: 600 }}>Most likely cause of death</h5></CardHeader>
                             <LollipopChart data={summaryViewData.probabiliiesOfDyingOfEachDeathCause} descriptions={this.props.store.loadedDataStore.descriptions} formatting={probabilityLollipopChartFormatter}/>
                         </Card>
                     </Col>
-                </Row>
+                </Row> */}
             </Fragment>
         )
     }
